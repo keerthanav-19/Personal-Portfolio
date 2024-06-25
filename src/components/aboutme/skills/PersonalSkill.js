02bbe8d5a5a157c6dfc916b8f7e0196ebcdbef60
Keerthana V <keerthanachinnu54@gmail.com>
2024-06-25 11:37:17 +0530

    Update PersonalSkill.js

--- src/components/aboutme/skills/PersonalSkill.js
+++ src/components/aboutme/skills/PersonalSkill.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { Container,Row,Col } from 'react-bootstrap';
+import { Container,Row } from 'react-bootstrap';
 import LightSpeed from 'react-reveal/LightSpeed';
 
 export default function PersonalSkill() {
